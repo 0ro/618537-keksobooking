76350120dc6cae9fd8705386721735cc45d4b5c7
doooob <den.bogdanov4@gmail.com>
2018-02-11 15:33:22 +0300

    Replace switch/case on Map

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 const args = process.argv.slice(2);
 const version = require(`./src/version`);
 const help = require(`./src/help`);
-const error = require(`./src/error`);
 const author = require(`./src/author`);
 const license = require(`./src/license`);
 const description = require(`./src/description`);
 const voidCommand = require(`./src/voidCommand`);
+const error = require(`./src/error`);
+const map = new Map([
+  [`--${version.name}`, version.execute],
+  [`--${help.name}`, help.execute],
+  [`--${author.name}`, author.execute],
+  [`--${license.name}`, license.execute],
+  [`--${description.name}`, description.execute],
+  [void 0, voidCommand.execute]
+]);
 
-switch (args[0]) {
-  case `--version`:
-    version.execute();
-    break;
-
-  case `--help`:
-    help.execute();
-    break;
-
-  case `--author`:
-    author.execute();
-    break;
-
-  case `--license`:
-    license.execute();
-    break;
-
-  case `--description`:
-    description.execute();
-    break;
-
-  case void 0:
-    voidCommand.execute();
-    break;
-
-  default:
-    error.execute(args.join(` `));
+if (map.get(args[0])) {
+  map.get(args[0])();
+} else {
+  error.execute(args.join(` `));
 }
