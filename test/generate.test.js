e7804f16190ec4c6a60159f29da72ab07d027953
doooob <den.bogdanov4@gmail.com>
2018-02-20 01:12:46 +0300

    Fix close

--- test/generate.test.js
+++ test/generate.test.js
@@ -2,13 +2,16 @@ const {isFileExist, createFileWithResult} = require(`../src/generate`);
 const fs = require(`fs`);
 const {promisify} = require(`util`);
 const unlink = promisify(fs.unlink);
+const close = promisify(fs.close);
 
 describe(`Generate JSON command`, function () {
   it(`should create new file`, function () {
     const fileName = `testfile`;
     const numberOfEntity = 10;
     return isFileExist({fileName, numberOfEntity})
+        .then((fd) => close(fd))
         .then(createFileWithResult.bind(null, {fileName, numberOfEntity}))
-        .then(() => unlink(`${process.cwd()}/${fileName}.json`));
+        .then(() => unlink(`${process.cwd()}/${fileName}.json`))
+        .catch((err) => console.error(err));
   });
 });
